53df05a14b3d2b4b5ed295d34b3cc158eb6a81fc
uncleoptimus <uncle.optimus@gmail.com>
2015-02-13 23:54:50 -0800

    can now select a cat, its info appears in Jumbotron element. Info is interconnected with selected Kat model

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 A cat clicker app with several iterations. Built with Knockout and jQuery.
+Initial version defaults to two cats with independent click counters, a button to add more cats, and another button to simultaneously increcment all cat's counters. App uses Namely to randomly assign a name for each new cat.
 
+Version 2 will use a selection mechanism to display chosen cat's info and will begin with 5 cats. I will use Knockout to also allow editing of chosen cat's name. Also plan to use Flickr api to randomly assign each cat a pic.
--- scripts/main2.js
+++ scripts/main2.js
@@ -9,17 +9,37 @@ function Kat(given_name) {
 			self.name = data[0];
 		});
 
-	self.klikcount = ko.observable(0);
+	self.klikcount = ko.observable(0).extend({ notify: 'always' });;
+//	self.klikcount.extend({ notify: 'always' });
 }
 
 function KlikKatViewModel() {
 	var self = this;
+	self.chosen_cat = ko.observable(0); // keep track of selected kitty
+	self.current_kat = ko.observable();
 
 	// kats...yup can be more than one...
 	self.kats = ko.observableArray([
 		new Kat("Feenie"),
 		new Kat("Pearls")
 	]);
+	self.current_kat(self.kats()[0]);
+
+//	self.klikcount = ko.observable(0);
+	self.klikcount = ko.computed(function() {
+		return self.kats()[self.chosen_cat()].klikcount();
+	});
+
+
+
+	self.selectKat = function(index, kat, event) {
+		console.log("Chosen kat is %s", index);
+		self.chosen_cat(index);
+
+		var chosen_cat_count = self.kats()[self.chosen_cat()].klikcount();
+		self.kats()[self.chosen_cat()].klikcount(chosen_cat_count);
+//		self.klikcount(kat.klikcount());
+	}
 
 	self.spawnKat = function() {
 		namey.get(function(data) {
@@ -29,17 +49,26 @@ function KlikKatViewModel() {
 		});
 	}
 
-	self.counterUp = function(kat) {
-		console.log("counter up for kat...%O", kat );
-		var current_count = kat.klikcount();
-		kat.klikcount(current_count+1);
+//	self.counterUp = function(kat) {
+//		console.log("counter up for kat...%O", kat );
+//		var current_count = kat.klikcount();
+//		kat.klikcount(current_count+1);
+//	}
+
+	self.counterUp = function() {
+		var chosen_cat_count = self.kats()[self.chosen_cat()].klikcount();
+		self.kats()[self.chosen_cat()].klikcount(chosen_cat_count + 1);
+//		self.klikcount(self.klikcount()+1);
 	}
 
 	self.allUp = function() {
 		console.log("Updating counters...for total cats %s", self.kats().length);
 		for(i=0; i < self.kats().length; i++) {
 			var current_count = self.kats()[i].klikcount();
+//			if(i === chosen_cat) self.klikcount(self.klikcount() + 1);
+//			else
 			self.kats()[i].klikcount(current_count+1);
+
 		}
 	}
 }
